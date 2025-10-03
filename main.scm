8eba20f4a7ac2bb8b8e991c90805236444cda2df
Christophe Pelé <97438970+cpele@users.noreply.github.com>
2025-10-03 08:24:59 +0200

    Fix start & stop functions

--- main.scm
+++ main.scm
@@ -16,8 +16,8 @@
 	  "Starting server\n"))
 
 ;; Define empty (undefined?) server thread & socket vars
-(define server-thread (make-undefined-variable))
-(define server-socket (make-undefined-variable))
+(define server-thread #f)
+(define server-socket #f)
 
 ;; Start-server function:
 ;; - Skip when server-thread is defined 
@@ -29,31 +29,43 @@
 ;; - Keep the thread in the server-thread var
 (define (start-server)
   (unless server-socket
+    (display "Setting up server socket...\n")
     (set! server-socket (socket AF_INET SOCK_STREAM 0))
     (setsockopt server-socket SOL_SOCKET SO_REUSEADDR 1)
-    (bind server-socket INADDR_LOOPBACK 8080)
-    (listen server-socket 128))
+    (bind server-socket AF_INET INADDR_LOOPBACK 8080)
+    (listen server-socket 128)
+    (display "Server socket set up\n"))
   (unless server-thread
+    (display "Setting up server thread...\n")
     (set! server-thread
 	  (call-with-new-thread
 	   (lambda ()
-	     (run-server (lambda (request request-body)
-			   (handle-req request request-body))))))))
+	     (run-server
+	      (lambda (request request-body)
+		(handle-req request request-body))
+	      'http
+	      `(#:socket ,server-socket)))))
+    (display "Server thread set up\n")))
 
 ;; Stop-server function:
 ;; - Check for existing server socket
 ;; - Close the server socket
-;; - Clear the server-socket var
+;; - Clear the server-socket variable
 ;; - Check for existing server thread
 ;; - Join the server thread to let the server loop exit
 ;; - Clear the server-thread var
 (define (stop-server)
   (when server-socket
+    (display "Closing server socket...\n")
     (close server-socket)
-    (set! server-socket (make-undefined-variable)))
+    (display "Socket closed\n")
+    (set! server-socket #f))
   (when server-thread
+    (display "Waiting for server thread to terminate...\n")
+    (cancel-thread server-thread)
     (join-thread server-thread)
-    (set! server-thread (make-undefined-variable))))
+    (display "Server thread terminated\n")
+    (set! server-thread #f)))
 
 ;; Restart-server function: stop then start
 ;; TBD
